12cdd975d9ac299b24e829ef539fedb56d555472
Dan Serban <dserban01@yahoo.com>
2018-09-26 19:06:13 +0200

    Update runextractor.sh

--- bq_extractor/runextractor.sh
+++ bq_extractor/runextractor.sh
@@ -6,12 +6,13 @@ DATE_TO=$(cut -d'|' -f2 /opt/secrets/pipe_delimited_date_range.txt)
 GCP_PROJECT_ID=$(jq -r '.project_id' ${KEY_FILE_LOCATION})
 HDFS_PORT=9000
 FQ_BQ_AVRO_HDFS_DIR=hdfs://${MORPHL_SERVER_IP_ADDRESS}:${HDFS_PORT}/${BQ_AVRO_HDFS_DIR}
-DATA_ID=ga_sessions_$(echo ${DAY_OF_DATA_CAPTURE} | sed 's/-//g')
+GA_SESSIONS_DATA_ID=ga_sessions_$(echo ${DAY_OF_DATA_CAPTURE} | sed 's/-//g')
+WEBSITE_URL=$(</opt/secrets/website_url.txt)
 gcloud config set project ${GCP_PROJECT_ID}
 gcloud auth activate-service-account --key-file=${KEY_FILE_LOCATION}
 bq ls &>/dev/null
-bq extract --destination_format=AVRO ${SRC_BQ_DATASET}.${DATA_ID} gs://${DEST_GCS_BUCKET}/${DATA_ID}.avro
-gsutil cp gs://${DEST_GCS_BUCKET}/${DATA_ID}.avro /opt/landing/
+bq extract --destination_format=AVRO ${SRC_BQ_DATASET}.${GA_SESSIONS_DATA_ID} gs://${DEST_GCS_BUCKET}/${GA_SESSIONS_DATA_ID}.avro
+gsutil cp gs://${DEST_GCS_BUCKET}/${GA_SESSIONS_DATA_ID}.avro /opt/landing/
 hdfs dfs -mkdir -p ${FQ_BQ_AVRO_HDFS_DIR}
-hdfs dfs -copyFromLocal -f /opt/landing/${DATA_ID}.avro ${FQ_BQ_AVRO_HDFS_DIR}/${DATA_ID}.avro
-# rm /opt/landing/${DATA_ID}.avro
+hdfs dfs -copyFromLocal -f /opt/landing/${GA_SESSIONS_DATA_ID}.avro ${FQ_BQ_AVRO_HDFS_DIR}/${GA_SESSIONS_DATA_ID}.avro
+# rm /opt/landing/${GA_SESSIONS_DATA_ID}.avro
