03ef1b56684bae5b081176ea10106d2a66592510
Dan Serban <dserban01@yahoo.com>
2018-09-26 23:08:43 +0200

    Update runextractor.sh

--- bq_extractor/runextractor.sh
+++ bq_extractor/runextractor.sh
@@ -10,14 +10,17 @@ GCP_PROJECT_ID=$(jq -r '.project_id' ${KEY_FILE_LOCATION})
 HDFS_PORT=9000
 FQ_BQ_AVRO_HDFS_DIR=hdfs://${MORPHL_SERVER_IP_ADDRESS}:${HDFS_PORT}/${BQ_AVRO_HDFS_DIR}
 GA_SESSIONS_DATA_ID=ga_sessions_$(echo ${DAY_OF_DATA_CAPTURE} | sed 's/-//g')
+DEST_TABLE=${DEST_BQ_DATASET}.${GA_SESSIONS_DATA_ID}
+DEST_GCS_AVRO_FILE=gs://${DEST_GCS_BUCKET}/${GA_SESSIONS_DATA_ID}.avro
 WEBSITE_URL=$(</opt/secrets/website_url.txt)
 gcloud config set project ${GCP_PROJECT_ID}
 gcloud auth activate-service-account --key-file=${KEY_FILE_LOCATION}
 bq ls &>/dev/null
 sed "s/GCP_PROJECT_ID/${GCP_PROJECT_ID}/g;s/SRC_BQ_DATASET/${SRC_BQ_DATASET}/g;s/GA_SESSIONS_DATA_ID/${GA_SESSIONS_DATA_ID}/g;s/WEBSITE_URL/${WEBSITE_URL}/g" /opt/code/bq_extractor/query.sql.template > /opt/code/bq_extractor/query.sql
-bq query --use_legacy_sql=false --destination_table=${DEST_BQ_DATASET}.${GA_SESSIONS_DATA_ID} < /opt/code/bq_extractor/query.sql &>/dev/null
-bq extract --destination_format=AVRO ${DEST_BQ_DATASET}.${GA_SESSIONS_DATA_ID} gs://${DEST_GCS_BUCKET}/${GA_SESSIONS_DATA_ID}.avro
-gsutil cp gs://${DEST_GCS_BUCKET}/${GA_SESSIONS_DATA_ID}.avro /opt/landing/
+bq query --use_legacy_sql=false --destination_table=${DEST_TABLE} < /opt/code/bq_extractor/query.sql &>/dev/null
+bq extract --destination_format=AVRO ${DEST_TABLE} ${DEST_GCS_AVRO_FILE}
+echo ${DEST_TABLE} | grep ^bq_avro_morphl.ga_sessions_ && bq rm -f ${DEST_TABLE}
+gsutil cp ${DEST_GCS_AVRO_FILE} /opt/landing/
 hdfs dfs -mkdir -p ${FQ_BQ_AVRO_HDFS_DIR}
 hdfs dfs -copyFromLocal -f /opt/landing/${GA_SESSIONS_DATA_ID}.avro ${FQ_BQ_AVRO_HDFS_DIR}/${GA_SESSIONS_DATA_ID}.avro
 # rm /opt/landing/${GA_SESSIONS_DATA_ID}.avro
